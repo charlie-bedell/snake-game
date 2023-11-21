bb23dc145ff62c40f3553050326b43ef702a679d
Charles Bedell <charlesbedell5@gmail.com>
2023-11-21 11:20:53 -0500

    remove unsued imports; increase board size; lower tickspeed

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 import { createBoard, boardCenterId } from "./board.js";
-import { Player } from "./player.js";
-import { drawPlayer, redrawBoard, gameLoop } from "./game_logic.js";
+import { gameLoop } from "./game_logic.js";
 
 
-const HEIGHT = 15; // x or row
-const WIDTH = 15;  // y or column
-const TICKSPEED = 100;
+const HEIGHT = 20; // x or row
+const WIDTH = 20;  // y or column
+const TICKSPEED = 120;
 
 
 
 createBoard(HEIGHT, WIDTH);
-
 gameLoop(TICKSPEED, HEIGHT, WIDTH, boardCenterId(HEIGHT, WIDTH));
